ec275df49393a5774f65d4a3ac7a05e43586cda6
Bartosz Chrostek <bartek.chrostek@gmail.com>
2017-01-14 23:53:36 +0100

    Porządki w kodzie

--- app/src/main/java/pz/lab/scheduler/MainScreen.java
+++ app/src/main/java/pz/lab/scheduler/MainScreen.java
@@ -1,6 +1,5 @@
 package pz.lab.scheduler;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -13,28 +12,18 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
-        ///uruchomienie osi czasu
-
-        Intent i = new Intent(this,Timeline.class);
-        startActivity(i);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_screen, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
+        
         if (id == R.id.action_settings) {
             return true;
         }
--- app/src/main/java/pz/lab/scheduler/Timeline.java
+++ app/src/main/java/pz/lab/scheduler/Timeline.java
@@ -1,27 +1,11 @@
 package pz.lab.scheduler;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutCompat;
-import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.Gravity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
-import java.util.LinkedList;
 
 /**
  * Created by Czarny on 2017-01-09.
